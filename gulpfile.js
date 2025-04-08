a9724de8a3d07e4e08fb942492d803b7ddb5292e
SergeyT <0tx0rx@gmail.com>
2025-04-08 23:35:10 +0700

    оптимизация gulp скрипта

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const WEBPACK = require('webpack-stream');
 const SOURCE_DIR = 'al_fisher';
 
 
-GULP.task('clean', async () => {
+GULP.task('cleanup', async () => {
     if (!FS.existsSync('dist')) {
         return;
     }
@@ -14,25 +14,15 @@ GULP.task('clean', async () => {
 });
 
 GULP.task('js', () => {
-    return GULP.src(`${SOURCE_DIR}/js/main.js`)
-        .pipe(WEBPACK(require('./wp.config.js')))
-        .pipe(GULP.dest('dist/js'));
+    const file = `${SOURCE_DIR}/js/main.js`;
+    const webpack = WEBPACK(require('./wp.config.js'));
+    return GULP.src(file).pipe(webpack).pipe(GULP.dest('dist/js'));
 });
 
-GULP.task('manifest', () => {
-    return GULP.src(`${SOURCE_DIR}/manifest.json`)
-        .pipe(GULP.dest('dist'));
+GULP.task('assets', () => {
+    const files = [`${SOURCE_DIR}/manifest.json`, `${SOURCE_DIR}/icons/**/*.png`, './LICENSE'];
+    const options = {base: SOURCE_DIR, encoding: false, allowEmpty: true};
+    return GULP.src(files, options).pipe(GULP.dest('dist/'));
 });
 
-GULP.task('icons', () => {
-    return GULP.src(`${SOURCE_DIR}/icons/**/*.png`, {encoding: false})
-        .pipe(GULP.dest('dist/icons'));
-});
-
-GULP.task('license', () => {
-    return GULP.src('./LICENSE')
-        .pipe(GULP.dest('dist'));
-});
-
-
-GULP.task('build', GULP.series('clean', GULP.parallel('js', 'manifest', 'icons', 'license')));
+GULP.task('build', GULP.series('cleanup', GULP.parallel('js', 'assets')));
